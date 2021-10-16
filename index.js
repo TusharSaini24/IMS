48601d714afbc45a379e0de5706f83d1b9796d72
abhinav-shar <abhisharma.ind22shb@gmail.com>
2021-10-16 17:01:35 +0530

    public files

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
 
 require('dotenv').config()
 
 const PORT = process.env.PORT
+
+app.use(express.urlencoded({extended:false}))
+app.use(express.static('public'))
+
+app.set('view engine','ejs')
+app.set('views','views')
 
 app.get('/',(req,res) => {
     res.send("<h1>Hello</h1>")
